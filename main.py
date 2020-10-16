d182e7df6f978f27550752d51ae53782c043a4f9
Ricardo <kardopoly@gmail.com>
2020-10-16 18:53:16 -0300

    correção ortografica, usando pacote "black" para formatar o codigo

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import csv
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    flash,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt', 'csv'}
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"txt", "csv"}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route('/hello')
+@app.route("/hello")
 def hello_world():
-	return 'Hello, World!'
+    return "Hello, World!"
 
 
 # no pronpt do windows:
@@ -28,75 +36,76 @@ def hello_world():
 # * Debugger PIN: 664-881-666
 # * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template("index.html")
 
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
-	if request.method == 'POST':
-		# check if the post request has the file part
-		if 'file' not in request.files:
-			flash('Arquivo incompleto')
-			return redirect(request.url)
-		file = request.files['file']
-		# if user does not select file, browser also
-		# submit an empty part without filename
-		if file.filename == '':
-			flash('Nenhum arquivo selecionado')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			print("arquivo:", file.filename)
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', filename=filename))
-	return render_template('upload.html')
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("Arquivo incompleto")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            flash("Nenhum arquivo selecionado")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print("arquivo:", file.filename)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("uploaded_file", filename=filename))
+    return render_template("upload.html")
 
 
 # salva o arquivo na pasta uploads
-@app.route('/uploads/<filename>')
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
-	# csv_to_dic(filename)
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    # csv_to_dic(filename)
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
-@app.route('/mostra_arquivo', methods=['POST'])
+@app.route("/mostra_arquivo", methods=["POST"])
 def arquivo_processado():
-	filename = request.form['filename']
-	tipo = request.form['tipo']
-	print(filename, tipo)
-	line_count = 0
-	chaves = []
-	linhas = []
+    filename = request.form["filename"]
+    tipo = request.form["tipo"]
+    print(filename, tipo)
+    line_count = 0
+    chaves = []
+    linhas = []
 
-	with open("uploads/" + filename) as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=';')
+    with open("uploads/" + filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=";")
 
-		for row in csv_reader:
-			if line_count == 0:
-				print("total de colunas: ", len(row))
-				print(f'Nomes das colunas: {", ".join(row)}')
-				chaves = ", ".join(row)
-			else:
-				linhas.append(row)
-			# print(row)
-			line_count += 1
+        for row in csv_reader:
+            if line_count == 0:
+                print("total de colunas: ", len(row))
+                print(f'Nomes das colunas: {", ".join(row)}')
+                chaves = ", ".join(row)
+            else:
+                linhas.append(row)
+            # print(row)
+            line_count += 1
 
-	print(f'Total de linhas processadas: {line_count}.')
+    print(f"Total de linhas processadas: {line_count}.")
 
-	for lin in linhas:
-		print(lin[:10])
+    for lin in linhas:
+        print(lin[:10])
 
-	return render_template('arquivos.html', linhas=linhas)
+    return render_template("arquivos.html", linhas=linhas)
 
 
-@app.route('/arquivos')
+@app.route("/arquivos")
 def arquivos():
-	lst_files = os.listdir(UPLOAD_FOLDER)
-	print("lista de arquivos salvos: ", lst_files)
-	return render_template('arquivos.html', files=lst_files)
+    lst_files = os.listdir(UPLOAD_FOLDER)
+    print("lista de arquivos salvos: ", lst_files)
+    return render_template("arquivos.html", files=lst_files)
