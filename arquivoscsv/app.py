47f3a5915f522ec6d5dfa79e0830757bad742f97
Ricardo <kardopoly@gmail.com>
2020-10-29 21:03:22 -0300

    corigindo caminho do import no init do cli

--- arquivoscsv/app.py
+++ arquivoscsv/app.py
@@ -3,6 +3,8 @@ from .ext import site
 from .ext import toolbar
 from .ext import config
 from .ext import db
+from .ext import cli
+
 
 
 def create_app():
@@ -11,9 +13,10 @@ def create_app():
     Returns:
         [type]: [app]
     """
-    app = Flask(__name__)
-    config.init_app(app)
+    app = Flask(__name__) # topo da inicialização
+    config.init_app(app) # depois de iniciar a app
     db.init_app(app)
-    toolbar.init_app(app)
+    cli.init_app(app) # depois de iniciar o db
+    toolbar.init_app(app) # debug de paginas web, antes de inicar o site
     site.init_app(app)
     return app
--- arquivoscsv/ext/cli/__init__.py
+++ arquivoscsv/ext/cli/__init__.py
@@ -1,7 +1,16 @@
-from .ext.db import db
+from arquivoscsv.arquivoscsv.ext import db
 
 def init_app(app):
     @app.cli.command()
     def create_db():
         """cria o banco de dados"""
-        db.create_all()
+        try:
+            db.create_all()
+        except expression as identifier:
+            pass
+        
+                
+    @app.cli.command()
+    def listar_centros():
+        """lista centros"""
+        return "lista de centros"
